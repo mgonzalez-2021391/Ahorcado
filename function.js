7d77c0bedff4cc235a8d537e311790fb2319abc3
mgonzalez-2021391 <mgonzalez-2021391@kinal.edu.gt>
2024-01-19 08:34:10 -0600

    Se actualizó el archivo

---
+++ function.js
@@ -0,0 +1,3 @@
+var palabras = ["Algoritmo","Antivirus","Malware","Adware","Archivo","Aplicacion","Tecnologia","Chatear","Coookies","Datos"];
+let Random = palabras.length()*Math.random();
+console.log(Random);
